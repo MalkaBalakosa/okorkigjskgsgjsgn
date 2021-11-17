98ec7300174f4e20a89e71c9f062bde273420fc7
Malka Abqary Balakosa <92303560+MalkaBalakosa@users.noreply.github.com>
2021-11-17 13:54:40 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 // merah
 const ubahWarnaMerah= document.getElementById('merah');
 ubahWarnaMerah.onclick= function() {
-    document.body.setAttribute('class', 'merah');
+    document.body.classList.toggle('merah');
 }
 const ubahWarnaJingga= document.getElementById('jingga')
 ubahWarnaJingga.onclick= function() {
-    document.body.setAttribute('class', 'jingga');
+    document.body.classList.toggle('jingga');
 }
 const ubahWarnaKuning= document.getElementById('kuning');
 ubahWarnaKuning.onclick= function() {
-    document.body.setAttribute('class', 'kuning');
+    document.body.classList.toggle('kuning');
 }
 const ubahWarnaHijau= document.getElementById('hijau');
 ubahWarnaHijau.onclick= function() {
-    document.body.setAttribute('class', 'hijau')
+    document.body.classList.toggle('hijau');
 }
 const ubahWarnaBiru= document.getElementById('biru');
 ubahWarnaBiru.onclick=function() {
-    document.body.setAttribute('class', 'biru');
+    document.body.classList.toggle('biru');
 }
 const ubahWarnaUngu= document.getElementById('ungu');
 ubahWarnaUngu.onclick=function() {
-    document.body.setAttribute('class', 'ungu');
+    document.body.classList.toggle('ungu');
 }
+
+
 function tampilkan_nama(){
     document.getElementById("hasil").innerHTML = "<h3>Dylan jelek</h3>";
 }
